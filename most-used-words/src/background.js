24b5ff2366d89cb2549201d5f9f02b2d3939bbb8
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2020-09-30 21:30:15 -0300

    Módulo electron finalizado

--- most-used-words/src/background.js
+++ most-used-words/src/background.js
@@ -3,7 +3,9 @@
 import { app, protocol, BrowserWindow } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+import './backend/index'
 const isDevelopment = process.env.NODE_ENV !== 'production'
+
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
--- most-used-words/src/components/Home.vue
+++ most-used-words/src/components/Home.vue
@@ -1,30 +1,58 @@
 <template>
   <v-container fluid>
-      <div class="pills">
-          <Pill v-for="word in groupedWords" :key="word.name" 
-          :name="word.name" :amount="word.amount" />
-      </div>
+    <v-form>
+      <v-file-input
+        label="Selecione as legendas"
+        prepend-icon="mdi-message-text"
+        append-outer-icon="mdi-send"
+        outlined
+        multiple
+        chips
+        v-model="files"
+        @click:append-outer="processSubtitles"
+      />
+    </v-form>
+    <div class="pills">
+      <Pill
+        v-for="word in groupedWords"
+        :key="word.name"
+        :name="word.name"
+        :amount="word.amount"
+      />
+    </div>
   </v-container>
 </template>
 
 <script>
-import Pill from './Pill'
+import { ipcRenderer } from 'electron'
+window.ipcRenderer = ipcRenderer
+import Pill from "./Pill"
 export default {
-    components: {
-        Pill
+  components: {
+    Pill,
+  },
+  data: () => {
+    return {
+      files: [],
+      groupedWords: [],
+    };
+  },
+  methods: {
+    processSubtitles() {
+      const paths = this.files.map(f => f.path)
+      ipcRenderer.send('process-subtitles', paths)
+      ipcRenderer.on('process-subtitles', (event, resp) => {
+        this.groupedWords = resp;
+      }) 
     },
-    data: () => {
-        return {
-            groupedWords: [
-                { name: 'i', amount: 1234 },
-                { name: 'you', amount: 900 },
-                { name: 'he', amount: 853 },
-            ]
-        }
-    }
-}
+  },
+};
 </script>
 
 <style>
-
+.pills {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
 </style>
